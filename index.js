b63f7f7801afe8ec3a30b40e5fc9e71ad157cce7
Alexey <xarlamlexa@mail.ru>
2021-03-23 11:22:06 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const http  = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, 'public', req.url === '/' ? 'index.html' : req.url);
+  fs.readFile(filePath, (err, deta) => {
+    if (err) {
+      fs.readFile(path.join(__dirname, 'public', 'error.html'), (err, data) => {
+        if(err) {
+          res.writeHead(500);
+          res.end('Error');
+        } else {
+          res.writeHead(200, {
+            'Content-Type': 'text/html'
+          });
+          res.end(data);
+        }
+        
+      })
+    } else {
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      res.end(data)
+    }
+  })
+});
+
+server.listen(3000, () => {
+  console.log('server has been started')
+})
